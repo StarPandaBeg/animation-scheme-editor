6de8c003083f626893958e3538902f36d01b4e87
StarPandaBeg <kshmurakov@mail.ru>
2024-06-19 19:57:08 +0300

    feat (core): add context transformation before node render

--- packages/core/src/2d/node.ts
+++ packages/core/src/2d/node.ts
@@ -26,8 +26,21 @@ export class Node {
     initialize(this);
   }
 
+  public localToParent(): DOMMatrix {
+    const matrix = new DOMMatrix();
+    matrix.translateSelf(this.x(), this.y());
+    matrix.scaleSelf(this.scale.x(), this.scale.y());
+
+    return matrix;
+  }
+
   render(context: CanvasRenderingContext2D) {
+    context.save();
+
+    this.transformContext(context);
     this.draw(context);
+
+    context.restore();
   }
 
   protected draw(context: CanvasRenderingContext2D) {
@@ -44,5 +57,17 @@ export class Node {
     const children = this.children();
     if (Array.isArray(children)) return children;
     return [children];
+  }
+
+  protected transformContext(context: CanvasRenderingContext2D) {
+    const matrix = this.localToParent();
+    context.transform(
+      matrix.a,
+      matrix.b,
+      matrix.c,
+      matrix.d,
+      matrix.e,
+      matrix.f,
+    );
   }
 }
