233d9c8b54dbbaa062c9f42e9c15ade711a2bf76
StarPandaBeg <kshmurakov@mail.ru>
2024-06-20 16:45:16 +0300

    feat (core): add layout bounding box
    add properties to get corners position

--- packages/core/src/2d/layout.ts
+++ packages/core/src/2d/layout.ts
@@ -1,5 +1,5 @@
 import {vector2Accessor} from '../decorators';
-import {PossibleVector2, Vector2Accessor} from '../util';
+import {BBox, PossibleVector2, Vector2, Vector2Accessor} from '../util';
 import {Node, NodeProps} from './node';
 
 export interface LayoutProps extends NodeProps {
@@ -10,7 +10,44 @@ export class Layout extends Node {
   @vector2Accessor()
   public declare readonly size: Vector2Accessor;
 
+  public get leftTop() {
+    const offset = this.size().div(2);
+    return this.position().sub(offset);
+  }
+
+  public get rightTop() {
+    return this.leftTop.addX(this.size().x);
+  }
+
+  public get leftBottom() {
+    return this.rightBottom.addX(-this.size().x);
+  }
+
+  public get rightBottom() {
+    const offset = this.size().div(2);
+    return this.position().add(offset);
+  }
+
   constructor(props: LayoutProps = {}) {
     super(props);
+  }
+
+  public getBBox(): BBox {
+    const childrenBBox = this.getChildrenBBox();
+    const thisHasSize = !this.size().exactlyEquals(Vector2.zero);
+    const thisBBox = BBox.fromSizeCentered(this.size());
+
+    if (thisHasSize) {
+      thisBBox.position = this.leftTop;
+    }
+    return BBox.fromBBoxes(thisBBox, childrenBBox);
+  }
+
+  protected getChildrenBBox(): BBox {
+    const childrenBoxes = this.children().map(child => {
+      if (child instanceof Layout) return child.getBBox();
+      return new BBox();
+    });
+    return BBox.fromBBoxes(...childrenBoxes);
   }
 }
